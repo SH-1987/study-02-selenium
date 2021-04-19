850de3c4b47d3d8aa8e01dfafab106747fac191a
SH-1987 <hano.1987.10.27@gmail.com>
2021-04-19 22:59:40 +0900

    complete

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -1,5 +1,8 @@
 import os
 from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pandas as pd
 
@@ -12,7 +15,7 @@ def set_driver(driver_path, headless_flg):
 
     # ヘッドレスモード（画面非表示モード）をの設定
     if headless_flg == True:
-        options.add_argument('--headless')
+        options.add_argument('--headless') # 画面を表示せずに動作するモード
 
     # 起動オプションの設定
     options.add_argument(
@@ -29,12 +32,18 @@ def set_driver(driver_path, headless_flg):
 
 
 def main():
-    search_keyword = "高収入"
+    search_keyword = input('Enter word:') #"高収入"
     # driverを起動
+    # driverを起動
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    '''
     if os.name == 'nt': #Windows
         driver = set_driver("chromedriver.exe", False)
     elif os.name == 'posix': #Mac
         driver = set_driver("chromedriver", False)
+    else:
+        driver = set_driver("chromedriver_linux", False)
+    '''
     # Webサイトを開く
     driver.get("https://tenshoku.mynavi.jp/")
     time.sleep(5)
@@ -53,19 +62,65 @@ def main():
         "topSearch__text").send_keys(search_keyword)
     # 検索ボタンクリック
     driver.find_element_by_class_name("topSearch__button").click()
+    time.sleep(5)
 
-    # ページ終了まで繰り返し取得
-    exp_name_list = []
-    # 検索結果の一番上の会社名を取得
-    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+    cols =["会社名","仕事内容","対象となる方","勤務地","給与","初年度年収"]
+    df = pd.DataFrame(index=[], columns=cols)
 
-    # 1ページ分繰り返し
-    print(len(name_list))
-    for name in name_list:
-        exp_name_list.append(name.text)
-        print(name.text)
+    page = 1
+    while True:
+        # ページ終了まで繰り返し取得
+        exp_name_list = []
+
+        # 検索結果の一番上の会社名を取得
+        name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+        tableelem = driver.find_elements_by_class_name("tableCondition")
         
+        if len(name_list) == 0:
+            print('FINISH')
+            break
 
+        # 1ページ分繰り返し
+        # print(len(name_list))
+
+        print(str(page)+"ページ目")
+        for i in range(len(name_list)):
+            exp_name_list.append(name_list[i].text)
+            # 広告回避
+            if page == 1:
+                ths = tableelem[i+2].find_elements(By.TAG_NAME, "th")
+                tds = tableelem[i+2].find_elements(By.TAG_NAME, "td")
+            else:
+                ths = tableelem[i].find_elements(By.TAG_NAME, "th")
+                tds = tableelem[i].find_elements(By.TAG_NAME, "td")
+            
+            rec = [name_list[i].text]
+            cols = ["会社名"]
+            for j in range(len(tds)):
+                cols.append(ths[j].text)
+                rec.append(tds[j].text)
+            
+            print(cols)
+            print(rec)
+
+            df1 = pd.Series(rec, index=cols)    
+            df = df.append(df1, ignore_index=True)
+
+            print(name_list[i].text)
+            print("  " + ths[0].text)
+            print("  " + ths[1].text)
+            print("  " + ths[2].text)
+
+        print(df)
+        page += 1
+        try:
+            nextpage = driver.find_element_by_class_name("iconFont--arrowLeft").get_attribute("href")
+            driver.get(nextpage)
+            time.sleep(5)
+        except :
+            df.to_csv("data.csv", index=True)
+            print('FINISH')
+            break
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
 if __name__ == "__main__":
